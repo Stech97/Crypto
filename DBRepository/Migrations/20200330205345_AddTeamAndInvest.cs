90f255efe99bd8d7d958580590322bbe6bc4f7a9
Maxim Stepuchev <maksim.stepuchev@quorum.ru>
2020-03-31 00:06:21 +0300

    Add migration to new table

---
+++ DBRepository/Migrations/20200330205345_AddTeamAndInvest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DBRepository.Migrations
+{
+    public partial class AddTeamAndInvest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Investment",
+                columns: table => new
+                {
+                    InvestmentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Profit = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investment", x => x.InvestmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    TeamID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.TeamID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInfo_TeamID",
+                table: "UserInfo",
+                column: "TeamID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserInfo_Team_TeamID",
+                table: "UserInfo",
+                column: "TeamID",
+                principalTable: "Team",
+                principalColumn: "TeamID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserInfo_Team_TeamID",
+                table: "UserInfo");
+
+            migrationBuilder.DropTable(
+                name: "Investment");
+
+            migrationBuilder.DropTable(
+                name: "Team");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserInfo_TeamID",
+                table: "UserInfo");
+        }
+    }
+}
--- Models/Investment.cs
+++ Models/Investment.cs
@@ -2,7 +2,7 @@
 {
 	public class Investment
 	{
-		public int InvesmentID { get; set; }
+		public int InvestmentID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public string Profit { get; set; }
