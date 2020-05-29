0c7842d178b7c4137dcb584fde5766c921d59028
mozg1694 <wolfgamghast@gmail.com>
2020-05-29 16:37:15 +0300

    comingsoon build version
    generated production build files

---
+++ build/precache-manifest.870668695b4bccd96a40d5c0b6efdd1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2d102a0bd14cfa4ad9bc141e078726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f8342f7f0408cdfc6fc",
+    "url": "/static/css/main.dd1dbc3d.chunk.css"
+  },
+  {
+    "revision": "af92c463f93947645eba",
+    "url": "/static/js/2.7a1d77dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a1d77dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8342f7f0408cdfc6fc",
+    "url": "/static/js/main.1ed6fda7.chunk.js"
+  },
+  {
+    "revision": "0721bfb6380458590f82",
+    "url": "/static/js/runtime-main.3de71ab5.js"
+  },
+  {
+    "revision": "bd78f3d46a9709cd00dbea7e31126757",
+    "url": "/static/media/waveimage.1bbd13a1.png"
+  }
+]);
