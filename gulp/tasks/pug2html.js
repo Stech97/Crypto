89808a882ef4148408ba8c8af82550aad24e4a61
mozg1694 <wolfgamghast@gmail.com>
2020-05-31 17:40:34 +0300

    many changes

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -4,15 +4,17 @@ const pug = require('gulp-pug')
 const pugLinter = require('gulp-pug-linter')
 const htmlValidator = require('gulp-w3c-html-validator')
 const bemValidator = require('gulp-html-bem-validator')
+const pretty = require('gulp-pretty-html')
 const config = require('../config')
 
 module.exports = function pug2html(cb) {
-	return gulp.src('src/pages/*.pug')
+	return gulp.src('src/pages/dashboard.pug')
 		.pipe(plumber())
 		.pipe(pugLinter({ reporter: 'default' }))
 		.pipe(pug())
 		.pipe(htmlValidator())
 		.pipe(bemValidator())
+    .pipe(pretty())
 		.pipe(gulp.dest('build'))
 
 }
