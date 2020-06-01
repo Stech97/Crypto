38c534a3c79d96a9f854210963731d2a5da9fe3d
mozg1694 <wolfgamghast@gmail.com>
2020-06-01 21:09:40 +0300

    router bug fixed

---
+++ build/precache-manifest.7c25be349e276d1a4a491b412bbb46c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bad58e7b97881f9f768a53728c59276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed51440fe5e13257be00",
+    "url": "/static/css/main.edff7e9c.chunk.css"
+  },
+  {
+    "revision": "95b125732bcecc0f24ee",
+    "url": "/static/js/2.6400b4dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6400b4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed51440fe5e13257be00",
+    "url": "/static/js/main.e6c5967e.chunk.js"
+  },
+  {
+    "revision": "0721bfb6380458590f82",
+    "url": "/static/js/runtime-main.3de71ab5.js"
+  },
+  {
+    "revision": "15f18128cdd9761bd76efa727a41cea1",
+    "url": "/static/media/joinus.d5ff39c7.png"
+  },
+  {
+    "revision": "7fd37082085b2e95fc462913e669e5c7",
+    "url": "/static/media/stealth-logo.f2d5972d.png"
+  },
+  {
+    "revision": "bd78f3d46a9709cd00dbea7e31126757",
+    "url": "/static/media/waveimage.1bbd13a1.png"
+  },
+  {
+    "revision": "8845fa7728a9dea9675fbc794c48db7d",
+    "url": "/static/media/worldmap2.318490c7.png"
+  }
+]);
