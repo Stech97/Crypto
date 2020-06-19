115332e6c91a499631c04cb30390e683f530fd65
Wolfgang <wolfgamghast@gmail.com>
2020-06-19 23:17:12 +0300

    exchange popup development

--- src/dashboard/components/dashcontent/contentbalance/converter.jsx
+++ src/dashboard/components/dashcontent/contentbalance/converter.jsx
@@ -1,49 +1,57 @@
 import React, { Component, Fragment } from 'react'
 
-class ExchangeContainerUSDtoBTC extends Component {
+class ExchangeContainer extends Component {
 	render() {
-		const { isOpened, closeModal } = this.props
+		const { cur1, cur2, isOpened, closeModal } = this.props
+		
+		const curToString = (cur) => {
+			if (cur === 'btc') {
+				return 'Bitcoin'
+			} else if (cur === 'dol') {
+				return 'USD'
+			} else {
+				return 'DET'
+			}
+		}
 
 		return(
 			<div className={isOpened ? "exchangebox" : "none"}>
-				<div className="exchangeusdtobtc">
-				    <div className="exchangeusdtobtc-header exchange-bluetext">
-				      <h1>Exchange</h1>
+				<div className="exchange">
+				    <h1 className="exchange-header exchange-bluetext">
+				      Exchange
+				    </h1>
+				    <div className="exchange-closeimg">
+				      <img onClick={(isOpened) => closeModal(isOpened)} src="/img/close-icon.png" />
 				    </div>
-				    <div onClick={closeModal} className="exchangeusdtobtc-closeimg">
-				      <img src="/img/close-icon.png" />
-				    </div>
-				    <div className="exchangeusdtobtc-usdtobtc exchange-bluetext">
+				    <div className="exchange-subheader exchange-bluetext">
 				      <h2>From USD to Bitcoin</h2>
 				    </div>
-				    <div className="exchangeusdtobtc-rate exchange-greytext">
+				    <div className="exchange-rate exchange-greytext">
 				      <h3>
 				        Exchange Rate
-				        <br />1 BTC = $7,2309
+				        <br />
+				        1 BTC = $7,2309
 				      </h3>
 				    </div>
-				    <div className="exchangeusdtobtc-usdammountheader exchange-bluetext">
-				      <h3>Insert USD Amount</h3>
-				    </div>
-				    <form
-				      className="exchangeusdtobtc-usdammount exchange-form"
-				      type="text"
-				      placeholder="1,05241231"
-				    ></form>
-				    <div className="exchangeusdtobtc-exchangeimg">
-				      <img src="exchange.svg" />
-				    </div>
-				    <div className="exchangeusdtobtc-btcammountheader exchange-bluetext">
-				      <h3>Bitcoin Amount</h3>
-				    </div>
-				    <form
-				      className="exchangeusdtobtc-btcammount exchange-form"
-				      type="text"
-				      placeholder="= $7,212"
-				    ></form>
-				    <div className="exchangeusdtobtc-exchangebutton">
-				      <div className="exchangeusdtobtc-exchangebutton-text">Exchange</div>
-				    </div>
+				    <form className="exchange-form">
+						<h5 className="exchange-form-label1">Insert USD Amount</h5>
+						<input
+					      className="exchange-form-cur1 exchange-form-input"
+					      type="text"
+					      placeholder="BTC"
+					    >
+				    	</input>
+				    	<div className="exchange-form-img">
+					    	<img src="/img/exchange-icon.png" />
+					    </div>
+					    <h5 className="exchange-form-label2">Bitcoin Amount</h5>
+					    <input
+					    	className="exchange-form-cur2 exchange-form-input"
+					    	type="text"
+					    	placeholder="$$$"
+					    ></input>
+					    <button className="exchange-form-button">Exchange</button>
+				    </form>
 				</div>
 			</div>
 		)
@@ -53,46 +61,21 @@ class ExchangeContainerUSDtoBTC extends Component {
 export default class ContentBalanceConverter extends Component {
 	
 	state = {
-		leftModal: {
-			isOpened: false,
-		},
-		rightModal: {
-			isOpened: false,
-		}
+		leftModal: false,
+		rightModal: false,
 	}
 	
 	toggleLeft = () => {
 		this.setState({
 			...this.state,
-			leftModal: {
-				isOpened: true,
-			},
+			leftModal: !this.state.leftModal,
 		})
 	}
 
 	toggleRight = () => {
 		this.setState({
 			...this.state,
-			rightModal: {
-				isOpened: false,
-			},
-		})
-	}
-	
-	closeLeft() {
-		console.log('######')
-		this.setState({
-			leftModal: {
-				isOpened: !this.state.leftModal.isOpened,
-			}
-		})
-	}
-
-	closeRight() {
-		this.setState({
-			rightModal: {
-				isOpened: !this.state.rightModal.isOpened,
-			}
+			rightModal: !this.state.rightModal,
 		})
 	}
 
@@ -100,18 +83,30 @@ export default class ContentBalanceConverter extends Component {
 		const { currency1, currency2 } = this.props
 		return (
 			<div className={"content-balance-" + currency1 + "-" + currency2 }>
-				<div onClick={this.toggleLeft} className="content-balance-arrow-left" >
+				<div onClick={() => this.toggleLeft()} className="content-balance-arrow-left" >
 					<svg viewBox="0 0 31 56">
 						<use href="#arrow-left" />
 					</svg>
-					<ExchangeContainerUSDtoBTC closeModal={() => this.closeLeft()} isOpened = {this.state.leftModal.isOpened}/>
 				</div>
-				<div onClick={this.toggleRight} className="content-balance-arrow-right">
+				<ExchangeContainer
+					key={1}
+					closeModal={() => this.toggleLeft()}
+					isOpened={this.state.leftModal}
+					cur1 = {currency2}
+					cur2 = {currency1}
+				/>
+				<div onClick={() => this.toggleRight()} className="content-balance-arrow-right">
 					<svg viewBox="0 0 31 56">
 						<use href="#arrow-right" />
 					</svg>
-					<ExchangeContainerUSDtoBTC closeModal={() => this.closeRight()} isOpened = {this.state.rightModal.isOpened}/>
 				</div>
+				<ExchangeContainer
+					key = {2}
+					closeModal={() => this.toggleRight()}
+					isOpened={this.state.rightModal}
+					cur1 = {currency1}
+					cur2 = {currency2}
+				/>
 			</div>
 		)
 	}
