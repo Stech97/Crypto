bf6667a053bd0129f26b795ea2c472fa35a53a9c
subscripti19 <crimeacs@yandex.ru>
2020-06-30 03:32:05 +0300

    fix #106

--- src/main/components/Feedback.jsx
+++ src/main/components/Feedback.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 const Feedback = () => {
-	return(
-		<section className="feedback">
-		  <div className="wrapper feedback-wrapper">
-		    <div className="feedback-header">
-		      <h1>Need some help?</h1>
-		    </div>
-		    <div className="feedback-button">
-		      <a href="#">Contact us</a>
-		    </div>
-		  </div>
-		</section>
-	)
-}
+  return (
+    <section className="feedback">
+      <div className="wrapper feedback-wrapper">
+        <div className="feedback-header">
+          <h1>Need some help?</h1>
+        </div>
+        <div className="feedback-button">
+          <a href="mailto:support@defima.io">Contact us</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Feedback
+export default Feedback;
