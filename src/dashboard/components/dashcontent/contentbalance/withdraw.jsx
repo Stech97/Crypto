50b83ef63ae110aaaa0c880377cf6d4f6d0214fb
subscripti19 <crimeacs@yandex.ru>
2020-07-01 06:59:12 +0300

    minor layout fix popup

--- src/dashboard/components/dashcontent/contentbalance/withdraw.jsx
+++ src/dashboard/components/dashcontent/contentbalance/withdraw.jsx
@@ -1,74 +1,63 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class WithdrawForm extends Component {
-	render() {
-		return (
-			<form action="" className="popup-withdraw-form">
-				<div className="popup-withdraw-form-header">
-					<img src="" alt="" />
-					<h2>Min. withdraw </h2>
-				</div>
-				<div className="popup-withdraw-form-input">
-					<label htmlFor="">Your bitcoin address</label>
-					<input type="text" />
-					<p className="error popup-withdraw-form-error"></p>
-				</div>
-				<div className="popup-withdraw-form-input">
-					<label htmlFor="">Insert Withdraw amount </label>
-					<input type="number" step="any" min="10" />
-					<p className="error popup-withdraw-form-error"></p>
-				</div>
-				<div className="popup-withdraw-form-button">
-					<button>Withdraw now</button>
-				</div>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form action="" className="popup-withdraw-form">
+        <div className="popup-withdraw-form-header">
+          <img src="" alt="" />
+          <h2>Min. withdraw $10</h2>
+        </div>
+        <div className="popup-withdraw-form-input">
+          <label htmlFor="">Your bitcoin address</label>
+          <input type="text" />
+          <p className="error popup-withdraw-form-error"></p>
+        </div>
+        <div className="popup-withdraw-form-input">
+          <label htmlFor="">Insert Withdraw amount </label>
+          <input type="number" step="any" min="10" />
+          <p className="error popup-withdraw-form-error"></p>
+        </div>
+        <div className="popup-withdraw-form-button">
+          <button>Withdraw now</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 class Withdraw extends Component {
-	render() {
-		const { handleClick, isOpened } = this.props;
+  render() {
+    const { handleClick, isOpened } = this.props;
 
-		return (
-			<div className={isOpened ? "popup" : "none"}>
-				<div
-					className="popup-layer"
-					onClick={() => handleClick()}
-				></div>
-				<div className="popup-wrapper">
-					<div className="popup-wrapper-header">
-						<h1>Add Funds</h1>
-					</div>
-					<div className="popup-wrapper-cross">
-						<img
-							onClick={() => handleClick()}
-							src="/img/close-icon.png"
-						/>
-					</div>
-					<div className="popup-wrapper-content popup-withdraw">
-						<div className="popup-withdraw-text">
-							<p>
-								To withdraw bitcoin, please let us know your
-								bitcoin address below and your exact amount you
-								want to withdraw. Withdraw requests will be
-								executed every Monday and credited on your
-								wallet within 72hours.
-							</p>
-						</div>
-						<div className="popup-withdraw-min">
-							<img
-								src="/img/add-funds-icon.png"
-								alt="add-funds"
-							/>
-							<p>Min. amount for product $100</p>
-						</div>
-						<WithdrawForm closeForm={handleClick} />
-					</div>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className={isOpened ? 'popup' : 'none'}>
+        <div className="popup-layer" onClick={() => handleClick()}></div>
+        <div className="popup-wrapper">
+          <div className="popup-wrapper-header">
+            <h1>Withdraw Bitcoin</h1>
+          </div>
+          <div className="popup-wrapper-cross">
+            <img onClick={() => handleClick()} src="/img/close-icon.png" />
+          </div>
+          <div className="popup-wrapper-content popup-withdraw">
+            <div className="popup-withdraw-text">
+              <p>
+                To withdraw bitcoin, please let us know your bitcoin address
+                below and your exact amount you want to withdraw. Withdraw
+                requests will be executed every Monday and credited on your
+                wallet within 72hours.
+              </p>
+            </div>
+            <div className="popup-withdraw-form-header-min">
+              <img src="/img/add-funds-icon.png" alt="add-funds" />
+            </div>
+            <WithdrawForm closeForm={handleClick} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Withdraw;
