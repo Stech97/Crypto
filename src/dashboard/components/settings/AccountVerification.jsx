09dd2623bf54b8a45b9e42861283e9f121a52158
subscripti19 <crimeacs@yandex.ru>
2020-07-01 20:08:10 +0300

    fix #135

--- src/dashboard/components/settings/AccountVerification.jsx
+++ src/dashboard/components/settings/AccountVerification.jsx
@@ -1,37 +1,37 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
 
 export default class AccountVerification extends Component {
-	render() {
-		return(
-			<div className="settings-verification">
-				<h3 className="settings-verification-header">
-					Account Verification (KYC)
-				</h3>
-				<div className="settings-verification-box settings-whitebox">
-					<h5 className="settings-verification-box-upload">
-						Upload Passport 
-						or National ID
-					</h5>
-					<button className="settings-verification-box-upload-gray-button">
-						Select file
-					</button>
-					<h5 className="settings-verification-box-proof">
-						Proof of address
-						e.g. phone or utilities bill
-					</h5>
-					<button className="settings-verification-box-proof-gray-button">
-						Select file
-					</button>
-					<h5 className="settings-verification-box-selfie">
-						Selfie with passport
-						or National ID						
-					</h5>
-					<button className="settings-verification-box-selfie-gray-button">
-						Select file
-					</button>
-					<button className="settings-verification-box-button settings-orange-button">Save</button>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="settings-verification">
+        <h3 className="settings-verification-header">
+          Account Verification (KYC)
+        </h3>
+        <div className="settings-verification-box settings-whitebox">
+          <h5 className="settings-verification-box-upload">
+            Upload Passport or National ID
+          </h5>
+          <button className="settings-verification-box-upload-gray-button">
+            Select file
+          </button>
+          <h5 className="settings-verification-box-proof">
+            Proof of address e.g. phone or utilities bill
+          </h5>
+          <button className="settings-verification-box-proof-gray-button">
+            Select file
+          </button>
+          <h5 className="settings-verification-box-selfie">
+            Selfie with passport or national ID + written letter with data of
+            upload
+          </h5>
+          <button className="settings-verification-box-selfie-gray-button">
+            Select file
+          </button>
+          <button className="settings-verification-box-button settings-orange-button">
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
