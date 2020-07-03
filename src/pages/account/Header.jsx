7a9f888c91e17c25a1f3442c5dea745915faba8b
crimeacs <crimeacs@yandex.ru>
2020-07-03 18:26:31 +0300

    Added user dropdown

--- src/pages/account/Header.jsx
+++ src/pages/account/Header.jsx
@@ -1,27 +1,110 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Grid } from '@material-ui/core/';
+
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
 
 export default function Header({ className, handleDrawerOpen, iconClassName }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="fixed" className={className}>
       <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerOpen}
-          edge="start"
-          className={iconClassName}
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
         >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap>
-          Dashboard
-        </Typography>
+          <Grid item>
+            <IconButton
+              color="#inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={iconClassName}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6" noWrap>
+              Dashboard
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              startIcon={<AssignmentIndIcon />}
+              aria-controls="customized-menu"
+              aria-haspopup="true"
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+            >
+              Username
+            </Button>
+            <StyledMenu
+              id="customized-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <StyledMenuItem>
+                <ListItemIcon>
+                  <ExitToAppIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
+              </StyledMenuItem>
+            </StyledMenu>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
