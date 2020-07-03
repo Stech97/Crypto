6fe2f0c777d8f4a37c3d4261f83e134ac48f032a
crimeacs <crimeacs@yandex.ru>
2020-07-03 12:23:28 +0300

    Added downloads

--- src/dashboard/components/history/content.jsx
+++ src/dashboard/components/history/content.jsx
@@ -1,9 +1,10 @@
-import React, { Component, Fragment } from "react";
-import { Helmet } from "react-helmet";
-import Plotly from "plotly.js-basic-dist";
-import createPlotlyComponent from "react-plotly.js/factory";
-import { connect } from "react-redux";
-import { getHistoryTable } from "../../actions/historyTable";
+import React, { Component, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import Plotly from 'plotly.js-basic-dist';
+import createPlotlyComponent from 'react-plotly.js/factory';
+import { connect } from 'react-redux';
+import { getHistoryTable } from '../../actions/historyTable';
+import CsvDownload from 'react-json-to-csv';
 
 const Plot = createPlotlyComponent(Plotly);
 
@@ -13,10 +14,10 @@ export const HistoryRecord = ({ record: { time, type, amount, balance } }) => {
       <h5 className="history-balance-content-row-column">{time}</h5>
       <h5 className="history-balance-content-row-column">{type}</h5>
       <h5 className="history-balance-content-row-column">
-        {"$ " + amount.toFixed(2)}
+        {'$ ' + amount.toFixed(2)}
       </h5>
       <h5 className="history-balance-content-row-column">
-        {"$ " + balance.toFixed(2)}
+        {'$ ' + balance.toFixed(2)}
       </h5>
     </div>
   );
@@ -46,54 +47,54 @@ class HistoryContent extends Component {
     const data = [
       {
         x: [
-          "01-05-2019 22:23:00",
-          "01-06-2019 22:23:00",
-          "01-07-2019 22:23:00",
-          "01-08-2019 22:23:00",
+          '01-05-2019 22:23:00',
+          '01-06-2019 22:23:00',
+          '01-07-2019 22:23:00',
+          '01-08-2019 22:23:00',
         ],
         y: [10, 15, 16, 19],
-        type: "scatter",
+        type: 'scatter',
         line: {
-          shape: "spline",
-          color: "#005C9F",
+          shape: 'spline',
+          color: '#005C9F',
         },
-        mode: "lines",
+        mode: 'lines',
       },
     ];
 
     const layout = {
       autoresize: true,
       margin: {
-        l: 5 + "%",
-        r: 5 + "%",
-        b: 25 + "%",
-        t: 5 + "%",
+        l: 5 + '%',
+        r: 5 + '%',
+        b: 25 + '%',
+        t: 5 + '%',
       },
 
-      plot_bgcolor: "#EFEFEF",
+      plot_bgcolor: '#EFEFEF',
       yaxis: {
-        title: "SUM",
+        title: 'SUM',
         titlefont: {
-          family: "IBM Plex Sans, sans-serif",
-          size: 1.5 + "rem",
-          color: "#838383",
+          family: 'IBM Plex Sans, sans-serif',
+          size: 1.5 + 'rem',
+          color: '#838383',
         },
-        side: "right",
+        side: 'right',
       },
       xaxis: {
         automargin: true,
-        title: "Time",
+        title: 'Time',
         titlefont: {
-          family: "IBM Plex Sans, sans-serif",
-          size: 1.5 + "rem",
-          color: "#838383",
+          family: 'IBM Plex Sans, sans-serif',
+          size: 1.5 + 'rem',
+          color: '#838383',
         },
-        ticks: "inside",
+        ticks: 'inside',
       },
       font: {
-        family: "IBM Plex Sans",
-        size: 1 + "rem",
-        color: "#838383",
+        family: 'IBM Plex Sans',
+        size: 1 + 'rem',
+        color: '#838383',
       },
     };
 
@@ -134,7 +135,11 @@ class HistoryContent extends Component {
               ))}
             </div>
           </div>
-          <h5 className="history-balance-download">Download Statement CSV</h5>
+          <CsvDownload
+            className="history-balance-download"
+            filename="balance.csv"
+            data={this.props.table.records}
+          />
         </div>
       </div>
     );
--- src/dashboard/components/investment/content.jsx
+++ src/dashboard/components/investment/content.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
-import { Helmet } from "react-helmet";
-import InvestmentProfit from "./investmentProfit";
-import InvestPopupForm from "./InvestForm";
-import InvestmentTable from "./InvestmentTable";
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import InvestmentProfit from './investmentProfit';
+import InvestPopupForm from './InvestForm';
+import InvestmentTable from './InvestmentTable';
+import CsvDownload from 'react-json-to-csv';
 
 class InvestmentDetails extends Component {
   state = {
@@ -19,8 +20,8 @@ class InvestmentDetails extends Component {
         <div
           className={
             this.state.isOpened
-              ? "investment-details-panel"
-              : "investment-details-panel investment-details-panel-closed"
+              ? 'investment-details-panel'
+              : 'investment-details-panel investment-details-panel-closed'
           }
         >
           <div className="investment-details-panel-header">
@@ -30,7 +31,7 @@ class InvestmentDetails extends Component {
             <svg
               onClick={() => this.handleClick()}
               role="img"
-              className={"arrow" + (this.state.isOpened ? "" : "-closed")}
+              className={'arrow' + (this.state.isOpened ? '' : '-closed')}
               preserveAspectRatio="xMinYMin slice"
               viewBox="0 0 25 15"
             >
@@ -40,7 +41,7 @@ class InvestmentDetails extends Component {
         </div>
         <div
           className={
-            this.state.isOpened ? "investment-details-content" : "none"
+            this.state.isOpened ? 'investment-details-content' : 'none'
           }
         >
           <p>
@@ -70,11 +71,11 @@ class InvestmentDetails extends Component {
 
 const InvestPopup = (props) => {
   return (
-    <div className={props.isOpened ? "popup" : "none"}>
+    <div className={props.isOpened ? 'popup' : 'none'}>
       <div onClick={() => props.closeModal()} className="popup-layer"></div>
       <div className="popup-wrapper">
         <div className="popup-wrapper-header">
-          <h1>{"Buy Product " + props.type}</h1>
+          <h1>{'Buy Product ' + props.type}</h1>
         </div>
         <div className="popup-wrapper-cross">
           <img
@@ -113,9 +114,9 @@ class InvestmentGood extends Component {
           <h4>{name}</h4>
         </div>
         <div className="investment-goods-box-content">
-          <p>{"Monthly Profit of up to " + percent + "% month"}</p>
-          <p>{"Starting from $" + invest}</p>
-          <p>{"Career commission qualified Level 1-" + levels}</p>
+          <p>{'Monthly Profit of up to ' + percent + '% month'}</p>
+          <p>{'Starting from $' + invest}</p>
+          <p>{'Career commission qualified Level 1-' + levels}</p>
         </div>
         <div
           onClick={() => this.toggleModal()}
@@ -138,19 +139,19 @@ class InvestmentGoods extends Component {
   render() {
     const goods = [
       {
-        name: "Small",
+        name: 'Small',
         percent: 6,
         invest: 100,
         levels: 2,
       },
       {
-        name: "Medium",
+        name: 'Medium',
         percent: 8,
         invest: 5000,
         levels: 4,
       },
       {
-        name: "Large",
+        name: 'Large',
         percent: 11,
         invest: 10000,
         levels: 7,
@@ -183,6 +184,11 @@ class InvestmentHistory extends Component {
           </div>
           <InvestmentTable />
         </div>
+        <CsvDownload
+          className="investment-balance-download"
+          filename="balance.csv"
+          data={InvestmentTable}
+        />
       </div>
     );
   }
