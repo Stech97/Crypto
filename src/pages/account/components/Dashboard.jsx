8476bc887d14fac02233dae394833decd62c6bc9
crimeacs <crimeacs@yandex.ru>
2020-07-05 10:28:32 +0300

    replaced material tables

--- src/pages/account/components/Dashboard.jsx
+++ src/pages/account/components/Dashboard.jsx
@@ -20,48 +20,13 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-import { forwardRef } from 'react';
-import MaterialTable from 'material-table';
-
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-};
+import { DataGrid, ToolbarOptions } from 'tubular-react';
+import {
+  AggregateFunctions,
+  ColumnDataType,
+  ColumnSortDirection,
+  createColumn,
+} from 'tubular-common';
 
 const drawerWidth = 240;
 
@@ -85,6 +50,67 @@ const rows_withdrawl = [
     </Button>,
   ),
 ];
+
+const rows_withdrawl_new = [
+  {
+    user: 'User 1',
+    amount: '0.1 BTC',
+    decision: 'Accept',
+  },
+  {
+    user: 'User 2',
+    amount: '0.2 BTC',
+    decision: 'Accept',
+  },
+];
+// DOCUMENTATION IS HERE: https://unosquare.github.io/tubular/tubular-react
+const toolbarOptions = new ToolbarOptions({
+  advancePagination: true,
+  bottomPager: true,
+  exportButton: true,
+  printButton: false,
+  searchText: true,
+  topPager: true,
+});
+
+const columns = [
+  createColumn('user', {
+    dataType: ColumnDataType.Text,
+    filterable: true,
+    isKey: true,
+    label: 'User',
+    sortDirection: ColumnSortDirection.Ascending,
+    sortOrder: 1,
+    sortable: true,
+  }),
+  createColumn('amount', {
+    dataType: ColumnDataType.Text,
+    filterable: true,
+    isKey: true,
+    label: 'Amount',
+    sortDirection: ColumnSortDirection.Ascending,
+    sortOrder: 1,
+    sortable: true,
+  }),
+  createColumn('decision', {
+    dataType: ColumnDataType.Text,
+    filterable: true,
+    isKey: true,
+    label: 'Decision',
+    sortDirection: ColumnSortDirection.Ascending,
+    sortOrder: 1,
+    sortable: true,
+  }),
+];
+
+const SampleGrid = () => (
+  <DataGrid
+    columns={columns}
+    dataSource={rows_withdrawl_new}
+    gridName="Grid"
+    toolbarOptions={toolbarOptions}
+  />
+);
 
 function createData_KYC(user, view, decision) {
   return { user, view, decision };
@@ -275,7 +301,16 @@ export default function Dashboard(props) {
           justify="space-between"
           alignItems="baseline"
         >
-          <Grid item xs={3}></Grid>
+          <Grid item xs={3}>
+            <Typography
+              className={classes.title}
+              variant="h5"
+              component="p"
+              gutterBottom
+            >
+              Withdrawl requests
+            </Typography>
+          </Grid>
           <Grid item xs={3}>
             <Button variant="contained" color="secondary">
               Bulk accept
@@ -288,74 +323,8 @@ export default function Dashboard(props) {
           justify="space-evenly"
           alignItems="center"
         >
-          <Grid item xs={12}>
-            <MaterialTable
-              icons={tableIcons}
-              title="Withdral requests"
-              columns={[
-                { title: 'User', field: 'user' },
-                { title: 'Ammount', field: 'ammount' },
-              ]}
-              data={[
-                {
-                  user: 'User 1',
-                  ammount: 'BTC 0.1',
-                },
-                {
-                  user: 'User 2',
-                  ammount: 'BTC 1.1',
-                },
-              ]}
-              actions={[
-                {
-                  icon: 'save',
-                  tooltip: 'Approve request',
-                  onClick: (event, rowData) =>
-                    alert('You saved ' + rowData.name),
-                },
-              ]}
-              components={{
-                Action: (props) => (
-                  <Button
-                    onClick={(event) => props.action.onClick(event, props.data)}
-                    color="secondary"
-                    variant="contained"
-                    style={{ textTransform: 'none' }}
-                    size="small"
-                  >
-                    Approve request
-                  </Button>
-                ),
-              }}
-            />
-            {/* <TableContainer component={Paper}>
-              <Table
-                className={classes.table}
-                size="small"
-                aria-label="a dense table"
-              >
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="right">User</TableCell>
-                    <TableCell align="right">Ammount</TableCell>
-                    <TableCell align="right">Decision</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows_withdrawl.map((rows_withdrawl) => (
-                    <TableRow key={rows_withdrawl.name}>
-                      <TableCell align="right">{rows_withdrawl.user}</TableCell>
-                      <TableCell align="right">
-                        {rows_withdrawl.amount}
-                      </TableCell>
-                      <TableCell align="right">
-                        {rows_withdrawl.decision}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer> */}
+          <Grid item xs={6}>
+            <SampleGrid />
           </Grid>
         </Grid>
 
