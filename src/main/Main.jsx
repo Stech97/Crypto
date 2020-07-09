a3f25553c3a8e8cafadb6a79e2b64e73b0fbc2a2
crimeacs <crimeacs@yandex.ru>
2020-07-09 11:43:31 +0300

    added Our Mission

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,17 +1,17 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
 //import "../styles/style.scss";
-import Header from "./components/Header";
-import Homescreen from "./components/Homescreen";
-import Ourmission from "./components/Ourmission";
-import Howitworks from "./components/Howitworks";
-import Portfolio from "./components/Portfolio";
-import Career from "./components/Career";
-import DefimaToken from "./components/Defimatoken";
-import Ourteam from "./components/Ourteam";
-import Joinus from "./components/Joinus";
-import Faq from "./components/Faq";
-import Feedback from "./components/Feedback";
-import Footer from "./components/Footer";
+import Header from './components/Header';
+import Homescreen from './components/Homescreen';
+import Ourmission from './components/Ourmission_new';
+import Howitworks from './components/Howitworks';
+import Portfolio from './components/Portfolio';
+import Career from './components/Career';
+import DefimaToken from './components/Defimatoken';
+import Ourteam from './components/Ourteam';
+import Joinus from './components/Joinus';
+import Faq from './components/Faq';
+import Feedback from './components/Feedback';
+import Footer from './components/Footer';
 
 class MainPage extends Component {
   render() {
