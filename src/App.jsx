06cb8c29dfe178bed605bda68c8916c8db7a55ca
Hast <wolfgamghast@gmail.com>
2020-08-17 18:50:27 +0300

    ip filtering

--- src/App.jsx
+++ src/App.jsx
@@ -11,60 +11,133 @@ import RouteWithSubRoutes from "./Routes";
 import { routes } from "./config";
 import { Helmet } from "react-helmet";
 import "./styles/common/main/global.scss";
-
+import ErrorPage from "./signup/ErrorPage";
+import axios from "axios";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
 // Create an enhanced history that syncs navigation events with the store
 
+const getIpFetch = () =>
+    axios.get("https://api.ipify.org?format=json", { mode: "cors" });
+
+const getCountryFetch = (ip) =>
+    axios.get("https://ipinfo.io/" + ip + "/?token=7a04a322ea8440");
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { status: false, loading: true };
+    }
+
+    CountryAllowed = async () => {
+        getIpFetch()
+            .then((req) => {
+                getCountryFetch(req.data.ip)
+                    .then((res) => {
+                        if (
+                            res.data.coumtry !== "US" &&
+                            res.data.coumtry !== "CA"
+                        ) {
+                            this.setState({
+                                ...this.state,
+                                status: true,
+                                loading: false,
+                            });
+                            console.log("1", this.state);
+                        } else {
+                            this.setState({
+                                ...this.state,
+                                status: false,
+                                loading: false,
+                            });
+                            console.log("2", this.state);
+                        }
+                    })
+                    .catch((error) => {
+                        this.setState({
+                            ...this.state,
+                            status: true,
+                            loading: false,
+                        });
+                        console.log("3", this.state);
+                    });
+            })
+            .catch((error) => {
+                this.setState({ ...this.state, status: true, loading: false });
+                console.log("4", this.state);
+            });
+    };
+
+    componentDidMount = async () => {
+        this.CountryAllowed();
+        this.forceUpdate();
+    };
+
     render() {
-        return (
-            <Router>
-                <Helmet>
-                    <title>Defima</title>
-                    <meta charSet="utf-8" />
-                    <meta name="description" content="Defima" />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="/site.webmanifest" />
-                    <link
-                        rel="mask-icon"
-                        href="/safari-pinned-tab.svg"
-                        color="#5bbad5"
-                    />
-                    <meta name="msapplication-TileColor" content="#da532c" />
-                    <meta name="theme-color" content="#ffffff" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
-                        rel="stylesheet"
-                    />
-                    <link
-                        rel="stylesheet"
-                        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-                        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-                        crossorigin="anonymous"
-                    />
-                </Helmet>
-                <Switch>
-                    {routes.map((route, i) => (
-                        <RouteWithSubRoutes key={i} {...route} />
-                    ))}
-                </Switch>
-            </Router>
-        );
+        if (this.state.loading) {
+            return (
+                <Backdrop>
+                    <CircularProgress color="red" />
+                </Backdrop>
+            );
+        } else {
+            if (this.state.status) {
+                return (
+                    <Router>
+                        <Helmet>
+                            <title>Defima</title>
+                            <meta charSet="utf-8" />
+                            <meta name="description" content="Defima" />
+                            <link
+                                rel="apple-touch-icon"
+                                sizes="180x180"
+                                href="/apple-touch-icon.png"
+                            />
+                            <link
+                                rel="icon"
+                                type="image/png"
+                                sizes="32x32"
+                                href="/favicon-32x32.png"
+                            />
+                            <link
+                                rel="icon"
+                                type="image/png"
+                                sizes="16x16"
+                                href="/favicon-16x16.png"
+                            />
+                            <link rel="manifest" href="/site.webmanifest" />
+                            <link
+                                rel="mask-icon"
+                                href="/safari-pinned-tab.svg"
+                                color="#5bbad5"
+                            />
+                            <meta
+                                name="msapplication-TileColor"
+                                content="#da532c"
+                            />
+                            <meta name="theme-color" content="#ffffff" />
+                            <link
+                                href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+                                rel="stylesheet"
+                            />
+                            <link
+                                rel="stylesheet"
+                                href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+                                integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+                                crossorigin="anonymous"
+                            />
+                        </Helmet>
+                        <Switch>
+                            {routes.map((route, i) => (
+                                <RouteWithSubRoutes key={i} {...route} />
+                            ))}
+                        </Switch>
+                    </Router>
+                );
+            } else {
+                return <ErrorPage />;
+            }
+        }
     }
 }
 
---
+++ src/signup/ErrorPage.jsx
@@ -0,0 +1,122 @@
+import React, { Fragment } from "react";
+//import "../styles/login.scss";
+
+import FluidContainer from "./Content";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+import { makeStyles } from "@material-ui/core/styles";
+
+const darkBlue = "#123273";
+const gradient = "linear-gradient(50deg, #123273 0%, #005c9f 100%)";
+const grayText = "#838383";
+const grayBack = "#efefef";
+const orange = "#ed7102";
+const lightBlue = "#16428d";
+const whitebox = "#efefef";
+const contentBack = "#f5fbff";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: contentBack,
+			contrastText: darkBlue,
+		},
+		secondary: {
+			main: "#fff",
+			contrastText: "#ffffff",
+		},
+		warning: {
+			main: "#fff",
+			dark: orange,
+		},
+	},
+	typography: {
+		fontFamily: ["IBM Plex Sans"],
+		h1: {
+			margin: 0,
+			lineHeight: 1.3,
+			letterSpacing: "normal",
+			textAlign: "left",
+			color: "#ffffff",
+			fontSize: "3rem",
+			fontWeight: 600,
+			fontStyle: "normal",
+		},
+
+		h2: {
+			margin: 0,
+			lineHeight: 1.3,
+			letterSpacing: "normal",
+			textAlign: "left",
+			color: "#005c9f",
+			fontSize: "1.75rem",
+			fontWeight: 600,
+			fontStyle: "normal",
+		},
+
+		h3: {
+			margin: 0,
+			lineHeight: 1.3,
+			letterSpacing: "normal",
+			textAlign: "left",
+			color: "#005c9f",
+			fontSize: "1.5rem",
+			fontWeight: 400,
+			fontStretch: "normal",
+			fontStyle: "italic",
+		},
+
+		h4: {
+			margin: 0,
+			lineHeight: 1.3,
+			letterSpacing: "normal",
+			textAlign: "left",
+			color: "#005c9f",
+			fontSize: "1.0rem",
+			fontWeight: 600,
+			fontStretch: "normal",
+		},
+
+		body1: {
+			fontSize: "1.0rem",
+			fontStretch: "normal",
+			fontStyle: "normal",
+			lineHeight: 1.32,
+			letterSpacing: "normal",
+			textAlign: "left",
+			color: "#838383",
+		},
+	},
+});
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		alignContent: "space-between",
+	},
+}));
+
+export default function SignupPage(props) {
+	const classes = useStyles();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<FluidContainer>
+				<Grid
+					xs={12}
+					md={8}
+					container
+					item
+					className={classes.container}
+					spacing={4}
+				>
+					<Typography variant="h1">
+						No access for your country.
+					</Typography>
+				</Grid>
+			</FluidContainer>
+		</ThemeProvider>
+	);
+}
